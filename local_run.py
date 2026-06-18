b1711108aeb9769b608a314cc5b819966f155196
instaai7281-blip <instaai7281-blip@users.noreply.github.com>
2026-06-18 22:56:05 +0530

    Update local_run.py with new bot token

--- local_run.py
+++ local_run.py
@@ -5,7 +5,7 @@ os.environ["PORT"] = "8080"
 os.environ["ENV"] = "true"
 os.environ["API_HASH"] = "11b88c331c5d44fde57cf91de1a2156b"
 os.environ["APP_ID"] = "27317669"
-os.environ["BOT_TOKEN"] = "8415514217:AAG3joSxSjYdb1nO4k56X5IwDJvQmFhtUPY"
+os.environ["BOT_TOKEN"] = "8146920201:AAHlctyHnhgLPQbqAon7gwoCLSnlDVC0cpc"
 os.environ["DATABASE_URL"] = "sqlite:///gdrive_bot.db"
 
 if __name__ == "__main__":
