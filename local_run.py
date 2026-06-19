1683cb306512ee07c6d25fd14fe2a0fa7a513d8d
instaai7281-blip <instaai7281-blip@users.noreply.github.com>
2026-06-19 10:23:31 +0530

    Update local_run.py with new bot token to bypass flood wait

--- local_run.py
+++ local_run.py
@@ -11,7 +11,7 @@ os.environ["PORT"] = "8080"
 os.environ["ENV"] = "true"
 os.environ["API_HASH"] = "11b88c331c5d44fde57cf91de1a2156b"
 os.environ["APP_ID"] = "27317669"
-os.environ["BOT_TOKEN"] = "8146920201:AAHlctyHnhgLPQbqAon7gwoCLSnlDVC0cpc"
+os.environ["BOT_TOKEN"] = "8053658721:AAE85g1ewKAqzs0QDWzhPO51dZlvW9sIn8A"
 os.environ["DATABASE_URL"] = "sqlite:///gdrive_bot.db"
 os.environ["ALT_PORT"] = "true"
 
